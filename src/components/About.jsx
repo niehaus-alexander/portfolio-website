975647e85022d28585df32629bad4eb8fb08d820
Alexander <alexander.niehaus@gmx.net>
2024-09-14 11:20:49 +0200

    add layout changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -11,14 +11,35 @@ const About = () => {
           </p>
         </div>
         <p className="text-xl mt-20">
-          After completing my Bachelor's degree in Economics with a focus on
-          Econometrics and Causal Analysis, I decided to pursue a career in
-          software development. Through a threemonth web development bootcamp
-          and intensive self-study, I learned to develop websites using React
-          and other modern technologies. My goal is to become a top-notch
-          full-stack developer, as I love continuously learning new things. In
-          my free time, I work on personal projects and deepen my knowledge in
-          backend technologies like Java and Spring.
+          With a Bachelor's degree in Economics, focusing on Econometrics and
+          Causal Analysis, I developed a strong foundation in data analysis and
+          problem-solving. However, my journey took an exciting turn when I
+          decided to dive into software development.
+          <br />
+          <br /> After completing a rigorous three-month web development
+          bootcamp and dedicating countless hours to self-study, I’ve become
+          proficient in building dynamic websites using React and other
+          cutting-edge technologies like Tailwind CSS. As I aim to become a
+          top-notch full-stack developer, I continuously seek to expand my skill
+          set. I have hands-on experience with backend technologies like
+          Node.js, MongoDB, RESTful APIs and i am currently starting to learn
+          Java and Spring.
+          <br /> <br /> My goal is to create seamless, efficient, and scalable
+          web applications that address real-world problems. What drives me is
+          not just the technical aspect of development but also the
+          collaborative environment that comes with it. I thrive in team
+          settings where diverse ideas and perspectives come together to build
+          something exceptional. My experiences have taught me the importance of
+          clear communication, flexibility, and mutual support in achieving
+          project goals.
+          <br /> <br /> In addition to my technical skills, I am an avid learner
+          and always eager to embrace new challenges. Whether it’s working on
+          personal projects or contributing to team initiatives, I am dedicated
+          to pushing the boundaries of what I can achieve and bringing value to
+          every project I undertake. Feel free to explore my portfolio to see
+          some of the projects I’ve worked on and get in touch if you’re
+          interested in collaborating or have any questions. I’m excited about
+          the opportunity to bring my skills and enthusiasm to your team!
         </p>
       </div>
     </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -31,9 +31,9 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full  "
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white pt-20">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
