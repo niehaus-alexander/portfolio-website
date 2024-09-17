132dd9a9e7416b86b99dc7bc65766774def1ba4b
Alexander <alexander.niehaus@gmx.net>
2024-09-17 17:11:32 +0200

    add own projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,22 @@
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import survivalpets from "../assets/portfolio/survivalpets.jpg";
+import portfolio from "../assets/portfolio/portfolio.jpg";
 
 const Portfolio = () => {
   const portfolios = [
-    { id: 1, src: arrayDestruct },
-    { id: 2, src: reactParallax },
-    { id: 3, src: navbar },
-    { id: 4, src: reactSmooth },
-    { id: 5, src: installNode },
-    { id: 6, src: reactWeather },
+    {
+      id: 1,
+      src: survivalpets,
+      demoLink:
+        "https://capstone-project-battle-pets-6l5c732ku-johannesbs-projects.vercel.app/",
+      githubLink:
+        "https://github.com/Braun-Johannes/Capstone-Project-BattlePets",
+    },
+    {
+      id: 2,
+      src: portfolio,
+      demoLink: "https://niehaus-alexander.netlify.app/",
+      githubLink: "https://github.com/niehaus-alexander/portfolio-website",
+    },
   ];
 
   return (
@@ -29,7 +33,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, demoLink, githubLink }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -37,12 +41,22 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href={demoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a
+                  href={githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Code
-                </button>
+                </a>
               </div>
             </div>
           ))}
